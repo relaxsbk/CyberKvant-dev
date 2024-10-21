c3038bd923c5928ace0cf8e71edb4b6ec053a452
relaxsbk.exe <pah4n1989@gmail.com>
2024-10-22 03:30:08 +0800

    новые икнонки + анимация

--- resources/js/components/Slider.vue
+++ resources/js/components/Slider.vue
@@ -14,10 +14,14 @@
             </div>
         </div>
         <button @click="prevSlide" class="absolute left-4 top-1/2 -translate-y-1/2">
-            Prev
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="gray" class="bi bi-caret-left-fill duration-200 ease-in transition-colors hover:fill-primary-purple" viewBox="0 0 16 16">
+                <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+            </svg>
         </button>
         <button @click="nextSlide" class="absolute right-4 top-1/2 -translate-y-1/2">
-            Next
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="gray" class="bi bi-caret-right-fill duration-200 ease-in transition-colors hover:fill-primary-purple" viewBox="0 0 16 16">
+                <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+            </svg>
         </button>
         <div class="flex justify-center ">
             <div class="indicators absolute bottom-4 flex justify-center items-center w-auto bg-[#1E1E1E] rounded-[10px] py-1 px-3 xs:mb-0 md:mb-10">
