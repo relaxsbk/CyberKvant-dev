d6dd2cfe4f15092d1e29940cab9583afaf8c1922
relaxsbk <pah4n1989@gmail.com>
2025-03-21 16:29:35 +0700

    Вынесен ярлык у карточки заказа

---
+++ resources/js/components/Profile/Order/LabelOrder.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    value: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <label class="bg-[#CA7FFE]/30 text-dark-purple px-6 py-1 rounded-[10px] text-sm">
+        {{value}}
+    </label>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/components/Profile/Order/OrderCard.vue
+++ resources/js/components/Profile/Order/OrderCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import OrderItem from "./OrderItem.vue";
+import LabelOrder from "./LabelOrder.vue";
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import OrderItem from "./OrderItem.vue";
         <div class="flex flex-row items-center gap-3 px-6 py-2 font-medium text-start rounded-t-lg border-b border-[#777777] text-black" id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
             <h1 class="text-xl text-black ">Заказ №1</h1>
             <span class="text-[#777777]">от 13.12.2024</span>
-            <label class="bg-[#CA7FFE]/30 text-dark-purple px-6 py-1 rounded-[10px] text-sm">Завершён</label>
+            <LabelOrder value="Завершён"/>
         </div>
         <div class="flex justify-between items-center gap-24 p-6 text-black">
             <div class="text-lg flex flex-row gap-5">
