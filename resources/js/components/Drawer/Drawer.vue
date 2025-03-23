98fd338cc7e1394f39cda1690dd9958480ccb31e
relaxsbk <pah4n1989@gmail.com>
2025-03-23 16:34:23 +0700

    Анимация дравера доделана форма авторизации с кнопокй запоминть меня и ссылкой забыл пароль

--- resources/js/components/Drawer/Drawer.vue
+++ resources/js/components/Drawer/Drawer.vue
@@ -1,67 +1,107 @@
 <script setup>
-
 import DrawerHead from "./DrawerHead.vue";
-import {useForm, Link} from "@inertiajs/vue3";
+import { useForm, Link } from "@inertiajs/vue3";
 import InputLabel from "../Forms/InputLabel.vue";
 import GroupInputLabel from "../Forms/GroupInputLabel.vue";
 import TextInput from "../Forms/TextInput.vue";
 import FormButton from "../Forms/FormButton.vue";
-import {inject, ref, watchEffect} from "vue";
+import {inject, ref, onMounted, provide} from "vue";
 
-const { closeDrawer } = inject('drawerActions')
+// Получаем функцию закрытия из провайдера
+const { closeDrawer: externalCloseDrawer } = inject('drawerActions');
 
+// Длительность анимации в мс
+const animationSpeed = 300;
+
+// Два состояния для анимации:
+// isVisible управляет наличием компонента в DOM (v-show)
+// isOpen отвечает за CSS-анимацию (сдвиг и прозрачность)
+const isVisible = ref(false);
 const isOpen = ref(false);
 
+// Функция открытия: сначала отображаем компонент, потом запускаем анимацию
+const open = () => {
+    isVisible.value = true;
+    // Небольшая задержка, чтобы Vue успел применить стили
+    setTimeout(() => {
+        isOpen.value = true;
+    }, 10);
+};
 
-watchEffect(() => {
-    isOpen.value = true;
+// Функция закрытия: сначала запускаем анимацию закрытия, а затем скрываем компонент
+const close = () => {
+    isOpen.value = false;
+    setTimeout(() => {
+        isVisible.value = false;
+        externalCloseDrawer();
+    }, animationSpeed);
+};
+
+onMounted(() => {
+    open();
 });
 
+
+// Пример использования useForm, если он вам нужен для формы
 const form = useForm({
     email: '',
     password: '',
-})
+    remember: '',
+});
 </script>
 
 <template>
+    <!-- Overlay с затемнением фона -->
     <div
-        @click="closeDrawer"
-        class="fixed w-full h-full top-0 left-0 z-40 bg-black transition-opacity duration-300 ease-out"
+        @click="close"
+        class="fixed w-full h-full top-0 left-0 z-40 bg-black transition-opacity"
+        :style="{ transitionDuration: animationSpeed + 'ms' }"
         :class="{ 'opacity-80': isOpen, 'opacity-0': !isOpen }"
+        v-show="isVisible"
     ></div>
+
+    <!-- Контент дравера -->
     <div
-        class="fixed top-0 right-0 z-50 h-full p-4 overflow-y-auto  w-96 bg-[#1E1E1E] transform transition-transform duration-300 ease-out"
+        class="fixed top-0 right-0 z-50 h-full p-4 overflow-y-auto w-96 bg-[#1E1E1E] transform transition-transform"
+        :style="{ transitionDuration: animationSpeed + 'ms' }"
         :class="{ 'translate-x-0': isOpen, 'translate-x-full': !isOpen }"
+        v-show="isVisible"
     >
-        <DrawerHead/>
+        <DrawerHead @close-drawer="close"/>
         <form>
             <GroupInputLabel class="mb-6">
-
                 <TextInput
                     id="email"
                     type="email"
                     v-model="form.email"
                     required
                 />
-                <InputLabel
-                    class=""
-                    for="email"
-                    value="Адрес электронной почты"
-                />
+                <InputLabel for="email" value="Адрес электронной почты" />
             </GroupInputLabel>
-            <GroupInputLabel class="">
+            <GroupInputLabel>
                 <TextInput
                     id="password"
                     type="password"
                     v-model="form.password"
                     required
                 />
-                <InputLabel
-                    for="password"
-                    value="Пароль"
-                />
+                <InputLabel for="password" value="Пароль" />
             </GroupInputLabel>
-            <FormButton value="Войти"/>
+            <div class="flex justify-between">
+                <div class="flex items-center gap-2">
+                    <input
+                        id="remember"
+                        type="checkbox"
+                        v-model="form.remember"
+                        class="mb-2 cursor-pointer text-primary-purple  rounded focus:ring-purple-600  ring-offset-gray-800 focus:ring-2 bg-gray-600 border-gray-600"
+                    />
+                    <label class="mb-2">Запомнить меня</label>
+                </div>
+                <Link class="text-gray-500 hoverText duration-200 ease-out">
+                    Забыли пароль?
+                </Link>
+            </div>
+            <FormButton value="Войти" />
         </form>
         <p class="text-center flex justify-center gap-2 mt-5">
             Нет аккаунта?
@@ -73,5 +113,5 @@ const form = useForm({
 </template>
 
 <style scoped>
-
+/* При необходимости добавьте дополнительные стили */
 </style>
--- resources/js/components/Drawer/DrawerHead.vue
+++ resources/js/components/Drawer/DrawerHead.vue
@@ -1,7 +1,6 @@
 <script setup>
-    import {inject} from "vue";
 
-    const { closeDrawer } = inject('drawerActions')
+    const emit = defineEmits(['closeDrawer'])
 </script>
 
 <template>
@@ -12,7 +11,7 @@
         </svg>
         Авторизация
     </h5>
-    <button @click="closeDrawer" type="button" data-drawer-hide="drawer-contact" aria-controls="drawer-contact" class="mt-3 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white duration-200" >
+    <button @click="() => emit('closeDrawer')" type="button" data-drawer-hide="drawer-contact" aria-controls="drawer-contact" class="mt-3 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white duration-200" >
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
         </svg>
